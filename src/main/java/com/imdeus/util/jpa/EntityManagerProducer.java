9c6d8b421ce2fe0a63f8f257e146bac35bd471b1
jkavdev <jhonatankolen@gmail.com>
2017-08-19 22:35:20 -0300

    Alterando escopo do EntityManagerProducer

--- src/main/java/com/imdeus/util/jpa/EntityManagerProducer.java
+++ src/main/java/com/imdeus/util/jpa/EntityManagerProducer.java
@@ -1,7 +1,7 @@
 package com.imdeus.util.jpa;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
@@ -17,8 +17,11 @@ public class EntityManagerProducer {
 		factory = Persistence.createEntityManagerFactory("ImDeusPU");
 	}
 
+	// com a nova versao do hibernate e cdi, o escopo request gera erro
+	// para resolver utilizaremos o dependent
+	// no qual pego o escopo que quem o chama
 	@Produces
-	@RequestScoped
+	@Dependent
 	public EntityManager createEntyManager() {
 		return factory.createEntityManager();
 	}
